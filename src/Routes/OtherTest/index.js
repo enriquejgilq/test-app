2ab4eb47cb190eb7d3902492b9b2d26cddbed789
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2022-07-18 14:40:14 -0500

    refactor suma de array

--- src/Routes/OtherTest/index.js
+++ src/Routes/OtherTest/index.js
@@ -3,21 +3,23 @@ import {arrayList} from './array'
 import ListNicknames from './ListNicknames';
 
 function index() {
-  let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-  //funcion que recibe un array como parametro para sumarlo
-  const test =(array)=>{
-    const initialValue = 0;
-    let sumWithInitial = array.reduce(
-      (previousValue, currentValue) => previousValue + currentValue,
-      initialValue
-    );
-    console.log(sumWithInitial);
+  const numerosPares= [1,2,3,4,5, 22, 21,24,26];
+  const fn1 = (numArr) => {
+  const filterArr = numArr.map(num => num > 20 && num % 2 === 0 ? 20 : num)
+  const values= filterArr.reduce((total, current) => total+current)
+    return (paramFn) => {
+      paramFn(values)
+    }
   }
+  
+  fn1(numerosPares)((numParFilt) => {
+    console.log(numParFilt)
+  })
   
   return (
     <div> 
-      <button onClick={()=>test(array)}> Click</button>
-      {/**componente React */}
+       {/**componente React */}
+       <p> ver console.log() para ver el otro ejercicio</p>
       <ListNicknames arrayList={arrayList} order='ASC'/>
     </div>
   )
